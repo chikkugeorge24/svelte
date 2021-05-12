654934bca2dcf8f11d7833145a765ee6bd24bb2d
Chikku <chikkugeorge24@gmail.com>
2021-05-12 08:34:41 +0530

    svelte binding to attributes

--- hello-world/src/App.svelte
+++ hello-world/src/App.svelte
@@ -2,12 +2,16 @@
 	const name = 'Chikku George'
 	const channel = '<b>Crafts Grimoire</b>'
 	const hack = `<a href="#" onClick="alert('You have been hacked')">Win a prize</a>`
+	const id = 'heading'
+	const disabled = true
 </script>
 
 <main>
 	<h1>Hello {name}</h1>
 	<div>{@html channel}</div>
 	<div>{@html hack}</div>
+	<h1 {id}>This is a heading</h1>
+	<button {disabled}>Bind</button>
 </main>
 
 <style>

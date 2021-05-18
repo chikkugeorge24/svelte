610bc896a176570b76e5b4dc770f331f30d7d94d
Chikku <chikkugeorge24@gmail.com>
2021-05-18 19:55:51 +0530

    svelte lists and keys

---
+++ svelte-demo/src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { shuffle } from "lodash";
+  let names = ["Alan", "Alex", "David", "Mathew"];
+  const shuffleNames = () => {
+    names = shuffle(names);
+  };
+</script>
+
+<main>
+  {#each names as name (name)}
+    <h2>{name}</h2>
+    <input placeholder="Last name" />
+  {/each}
+  <div>
+    <button on:click={shuffleNames}>Shuffle</button>
+  </div>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+</style>

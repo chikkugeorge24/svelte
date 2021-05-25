71dd032ad8ee3e3639912cdbe2a519da9905cd1f
Chikku <chikkugeorge24@gmail.com>
2021-05-25 08:22:10 +0530

    svelte component props

--- svelte-demo/src/App.svelte
+++ svelte-demo/src/App.svelte
@@ -1,9 +1,19 @@
 <script>
   import Greet from "./components/Greet.svelte";
+  const name = "Chikku",
+    channel = "Crafts Grimoire";
+  const obj = {
+    name: "Barry",
+    heroName: "Flash",
+  };
 </script>
 
 <main>
-  <Greet />
+  <Greet name="William" heroName="Batman" />
+  <Greet name="Clara" heroName="Wonderwomen" />
+  <Greet name="Clark" heroName="Batman" />
+  <Greet {name} />
+  <Greet {...obj} />
 </main>
 
 <style>
--- svelte-demo/src/components/Greet.svelte
+++ svelte-demo/src/components/Greet.svelte
@@ -1,1 +1,6 @@
-<h2>Hello Chikku George</h2>
+<script>
+  export let name;
+  export let heroName = "default";
+</script>
+
+<h2>Hello {name} a.k.a {heroName}</h2>

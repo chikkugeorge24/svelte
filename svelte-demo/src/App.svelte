86f9b7c2ed6b94a95c82d114755329f339f6e191
Chikku <chikkugeorge24@gmail.com>
2021-06-05 14:37:09 +0530

    svelte slot

--- svelte-demo/src/App.svelte
+++ svelte-demo/src/App.svelte
@@ -1,21 +1,46 @@
 <script>
-  import Button from "./components/Button.svelte";
-  import Outer from "./components/Outer.svelte";
-  const greetFunc = (event) => {
-    alert(event.detail);
-  };
-  const buttonClick = () => {
-    alert("clicked");
-  };
+  import Card from "./components/Card.svelte";
+  import NameList from "./components/NameList.svelte";
 </script>
 
 <main>
-  <Outer on:greet={greetFunc} />
-  <Button on:click={buttonClick} />
+  <!-- <Card>Card Content</Card>
+  <Card>
+    <h2>Card Content</h2>
+  </Card>
+  <Card>
+    <img src="https://picsum.photos/200" alt />
+  </Card>
+  <Card /> -->
+
+  <Card>
+    <div slot="header">
+      <h2>Card Header</h2>
+    </div>
+    <div slot="content">
+      <img src="https://picsum.photos/200" alt />
+    </div>
+    <!-- <div slot="footer">
+      <button>View Details</button>
+    </div> -->
+  </Card>
+
+  <NameList>
+    <h2 slot="hero" let:firstName let:lastName>{firstName} {lastName}</h2>
+  </NameList>
+
+  <NameList>
+    <h2 slot="hero" let:firstName let:lastName>{lastName} {firstName}</h2>
+  </NameList>
+
+  <NameList>
+    <h2 slot="hero" let:firstName let:lastName>{firstName}</h2>
+  </NameList>
 </main>
 
 <style>
   main {
+    text-align: center;
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
---
+++ svelte-demo/src/components/NameList.svelte
@@ -0,0 +1,20 @@
+<script>
+  const names = [
+    {
+      firstName: "Alan",
+      lastName: "Walker",
+    },
+    {
+      firstName: "Jhoney",
+      lastName: "Warner",
+    },
+    {
+      firstName: "Louis",
+      lastName: "Phelip",
+    },
+  ];
+</script>
+
+{#each names as name (name)}
+  <slot name="hero" firstName={name.firstName} lastName={name.lastName} />
+{/each}

133b897f9778c6e3a14fa131f94a85e2b84603ff
Chikku <chikkugeorge24@gmail.com>
2021-06-05 15:08:02 +0530

    component styles

--- svelte-demo/src/App.svelte
+++ svelte-demo/src/App.svelte
@@ -1,41 +1,11 @@
 <script>
-  import Card from "./components/Card.svelte";
-  import NameList from "./components/NameList.svelte";
+  import ChildStyles from "./components/ChildStyles.svelte";
 </script>
 
 <main>
-  <!-- <Card>Card Content</Card>
-  <Card>
-    <h2>Card Content</h2>
-  </Card>
-  <Card>
-    <img src="https://picsum.photos/200" alt />
-  </Card>
-  <Card /> -->
-
-  <Card>
-    <div slot="header">
-      <h2>Card Header</h2>
-    </div>
-    <div slot="content">
-      <img src="https://picsum.photos/200" alt />
-    </div>
-    <!-- <div slot="footer">
-      <button>View Details</button>
-    </div> -->
-  </Card>
-
-  <NameList>
-    <h2 slot="hero" let:firstName let:lastName>{firstName} {lastName}</h2>
-  </NameList>
-
-  <NameList>
-    <h2 slot="hero" let:firstName let:lastName>{lastName} {firstName}</h2>
-  </NameList>
-
-  <NameList>
-    <h2 slot="hero" let:firstName let:lastName>{firstName}</h2>
-  </NameList>
+  <h3>App Global component</h3>
+  <h4>App Component</h4>
+  <ChildStyles />
 </main>
 
 <style>
@@ -44,5 +14,11 @@
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
+  }
+  h4 {
+    color: orange;
+  }
+  :global(h3) {
+    color: blue;
   }
 </style>
---
+++ svelte-demo/src/components/ChildStyles.svelte
@@ -0,0 +1,9 @@
+<h4>Child Style compnent</h4>
+;
+<h3>Child Global style component</h3>
+
+<style>
+  h4 {
+    color: green;
+  }
+</style>

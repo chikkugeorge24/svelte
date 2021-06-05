5c04fd267d1315348213cce0aca4f34cded130fe
Chikku <chikkugeorge24@gmail.com>
2021-06-05 15:18:20 +0530

    svelte onMount hook in fetching http request

--- svelte-demo/src/App.svelte
+++ svelte-demo/src/App.svelte
@@ -1,22 +1,16 @@
 <script>
-  import ChildStyles from "./components/ChildStyles.svelte";
+  import PostList from "./components/PostList.svelte";
 </script>
 
 <main>
-  <h3>App Global component</h3>
-  <h4>App Component</h4>
-  <ChildStyles />
+  <PostList />
 </main>
 
 <style>
   main {
     text-align: center;
     padding: 1em;
-    max-width: 240px;
     margin: 0 auto;
-  }
-  h4 {
-    color: orange;
   }
   :global(h3) {
     color: blue;
---
+++ svelte-demo/src/components/PostList.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { onMount } from "svelte";
+  let posts = [];
+  onMount(async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    posts = await response.json();
+  });
+</script>
+
+{#each posts as post (post.id)}
+  <h3>{post.id}. {post.title}</h3>
+  <p>{post.body}</p>
+  <hr />
+{:else}
+  <p>Loading...</p>
+{/each}

025e34b3822541d19236817ced6e5358d41d3aab
Chikku <chikkugeorge24@gmail.com>
2021-06-06 20:49:20 +0530

    svelte dynamic tab component

--- svelte-demo/src/App.svelte
+++ svelte-demo/src/App.svelte
@@ -1,11 +1,24 @@
 <script>
-  import AutoFocus from "./components/AutoFocus.svelte";
-  import PostList from "./components/PostList.svelte";
+  import TabA from "./components/TabA.svelte";
+  import TabB from "./components/TabB.svelte";
+  import TabC from "./components/TabC.svelte";
+  let activeTab = TabA;
 </script>
 
 <main>
-  <!-- <PostList/> -->
-  <AutoFocus />
+  <button on:click={() => (activeTab = TabA)}>Tab A</button>
+  <button on:click={() => (activeTab = TabB)}>Tab B</button>
+  <button on:click={() => (activeTab = TabC)}>Tab C</button>
+  <svelte:component this={activeTab} />
+  <!-- {#if activeTab === "TabA"}
+    <TabA />
+  {/if}
+  {#if activeTab === "TabB"}
+    <TabB />
+  {/if}
+  {#if activeTab === "TabC"}
+    <TabC />
+  {/if} -->
 </main>
 
 <style>
---
+++ svelte-demo/src/components/TabB.svelte
@@ -0,0 +1,1 @@
+<h3>Tab B content</h3>
